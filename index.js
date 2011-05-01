207efa1e3b9a9f87783cde50a277cc17a4642c4a
James Halliday <mail@substack.net>
2011-05-01 05:09:18 -0700

    test is now passing yay

--- index.js
+++ index.js
@@ -30,48 +30,65 @@ module.exports = function (order) {
                 var next = links[i];
                 var cnext = clean(next);
                 
-                if (!db[cword]) db[cword] = {};
-                
-                var node = db[cword][cnext] || {
+                var node = db[cword] || {
                     count : 0,
                     words : {},
                     next : {},
                 };
-                db[cword][cnext] = node;
+                db[cword] = node;
                 
                 node.count ++;
                 node.words[word] = (node.words[word] || 0) + 1;
-                node.next[next] = (node.next[next] || 0) + 1;
+                node.next[cnext] = (node.next[cnext] || 0) + 1;
             }
+            
+            if (!db[cnext]) db[cnext] = {
+                count : 0,
+                words : {},
+                next : {},
+            };
+            db[cnext].words[next] = (db[cnext].words[next] || 0) + 1;
             
             if (cb) cb(null);
         }
     };
     
     self.respond = function (text, limit) {
+        if (!limit) limit = 100;
         var words = text.split(/\s+/);
         
         // find a starting point...
         var start = null;
-        var words = {};
+        var groups = {};
         for (var i = 0; i < words.length; i += order) {
             var word = clean(words.slice(i, i + 2).join(' '));
-            if (db[word]) {
-                start = deck.pick(Hash.map(db[word], function (node) {
-                    return node.count;
-                }));
-            }
+            if (db[word]) groups[word] = db[word].count;
         }
         
-        // nothing found, pick something random
-        if (!start) start = deck.pick(Object.keys(words));
+        var cur = Object.keys(groups).length > 0
+            ? deck.pick(groups)
+            : deck.pick(Object.keys(db))
+        ;
         
-        console.dir({ start : start });
+        var res = [];
+        
+        for (var i = 0; i < limit; i++) {
+            cur = deck.pick(db[cur].next);
+            if (!cur) break;
+            res.push(deck.pick(db[cur].words));
+        }
+        
+        return res;
     };
     
     return self;
 };
 
 function clean (s) {
-    return s.toLowerCase().replace(/[^a-z\d]+/g, '_');
+    return s
+        .toLowerCase()
+        .replace(/[^a-z\d]+/g, '_')
+        .replace(/^_/, '')
+        .replace(/_$/, '')
+    ;
 }
--- test/markov.js
+++ test/markov.js
@@ -4,5 +4,5 @@ var markov = require('markov');
 exports.order2 = function () {
     var m = markov(2);
     m.seed('This is a test.');
-    assert.eql(m.respond('This is'), 'a test.');
+    assert.eql(m.respond('This is'), [ 'a test.' ]);
 };
