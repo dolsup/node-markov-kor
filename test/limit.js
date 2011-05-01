3be99e3ce374c2ba0b4b6524bd6faa36495552fa
James Halliday <mail@substack.net>
2011-05-01 15:02:30 -0700

    failing limit test on lower limit bound

---
+++ test/limit.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+var markov = require('markov');
+var fs = require('fs');
+
+exports.limit = function () {
+    var to = setTimeout(function () {
+        assert.fail('never finished');
+    }, 5000);
+    
+    var m = markov(1);
+    
+    var these = 'the THE tHe ThE thE The the THE The tHE the the';
+    m.seed(these, function () {
+        clearTimeout(to);
+        
+        var counts = {};
+        for (var i = 0; i < 100; i++) {
+            var lim = Math.ceil(Math.random() * 10);
+            var res = m.respond('the', lim);
+            assert.ok(res.length <= lim);
+            
+            res.forEach(function (r) {
+                assert.ok(these.split(' ').indexOf(r) >= 0);
+                counts[r] = (counts[r] || 0) + 1;
+            });
+        }
+        
+        assert.eql(
+            Object.keys(counts).sort(),
+            these.split(' ').sort()
+        );
+    });
+};
