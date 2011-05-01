6d68ee2b5e19be8aba7f29ddad5f377e4f9f3890
James Halliday <mail@substack.net>
2011-05-01 06:05:01 -0700

    order1 passes too

--- test/markov.js
+++ test/markov.js
@@ -1,12 +1,24 @@
 var assert = require('assert');
 var markov = require('markov');
 
+exports.order1 = function () {
+    var m = markov(1);
+    m.seed('This is a test.');
+    assert.eql(
+        m.next('is'),
+        { word : 'a', key : 'a' }
+    );
+    assert.eql(
+        m.prev('is'),
+        { word : 'This', key : 'this' }
+    );
+};
+
 exports.order2 = function () {
     var m = markov(2);
     m.seed('This is a test.');
-    var key = m.search('This is');
     assert.eql(
-        m.next(key),
+        m.next('this_is'),
         { word : 'a test.', key : 'a_test' }
     );
     assert.eql(
