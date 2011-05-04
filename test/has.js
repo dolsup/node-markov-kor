e570b6852112cbce3ff66d21c512b28a9f6c607c
James Halliday <mail@substack.net>
2011-05-04 16:19:43 -0700

    __proto__ is still screwy but meh

---
+++ test/has.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+var markov = require('markov');
+var fs = require('fs');
+
+exports.has = function () {
+    var to = setTimeout(function () {
+        assert.fail('never finished');
+    }, 5000);
+    
+    var m = markov(1);
+    
+    var these = 'constructor __proto__ toLocaleString valueOf __defineGetter__';
+    m.seed(these, function () {
+        clearTimeout(to);
+        
+        var counts = {};
+        for (var i = 0; i < 100; i++) {
+            var res = m.respond('the', 100);
+            assert.ok(res.length < 100);
+            
+            res.forEach(function (r) {
+                assert.ok(these.split(' ').indexOf(r) >= 0);
+                counts[r] = (counts[r] || 0) + 1;
+            });
+        }
+        
+        assert.eql(
+            Object.keys(counts).sort(),
+            these.split(' ').sort()
+        );
+    });
+};
